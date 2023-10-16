322b58ed4c0c61d50fba42340150c4aae96501b0
EmanuelNordberg <122878887+DenAdlaMannen@users.noreply.github.com>
2023-10-16 12:00:32 +0200

    skräp

---
+++ BHZ/BHZ/Models/IMakeSound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHZ.Models
+{
+    public interface IMakeSound
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("This animal doesn't have a sound.,");
+        }
+    }
+}
---
+++ BHZ/BHZ/Models/Land.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHZ.Models
+{
+    public class Land : Animal, IMakeSound
+    {
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
