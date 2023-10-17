527db4a697f05647456e7b8337337788f294b251
EmanuelNordberg <122878887+DenAdlaMannen@users.noreply.github.com>
2023-10-17 11:43:34 +0200

    Initial Project

---
+++ BHZ/BHZ/Controllers/AnimalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BHZ.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult AnimalView()
+        {
+            return View();
+        }
+    }
+}
