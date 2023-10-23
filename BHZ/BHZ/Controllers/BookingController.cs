a42f54ae39441c9963284b6a57a2f4998d32b540
EmanuelNordberg <122878887+DenAdlaMannen@users.noreply.github.com>
2023-10-23 09:44:47 +0200

    BookAVisit

--- BHZ/BHZ/Controllers/BookingController.cs
+++ BHZ/BHZ/Controllers/BookingController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BHZ.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BHZ.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult Index()
+        private readonly BhzDbContext _context;
+
+        public BookingController(BhzDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult BookAVisit(int visitorId)
+        {
+            var visitor = _context.Visitors.Find(visitorId);
+
+            if(visitor != null)
+            {
+                return View(visitor);
+            }
+
+
+            return RedirectToAction("VisitorView");
+        }
+        public IActionResult DeleteAnimal(int animalID)
+        {
+
+            var animal = _context.Animals.Find(animalID);
+
+            if (animal != null)
+            {
+                _context.Animals.Remove(animal);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("AnimalView");
         }
     }
 }
